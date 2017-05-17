2fc37247850c00b1f99c2fc057af1aff3ceaf76d
markogrbic95 <marko_grbic@hotmail.com>
2017-05-17 17:13:03 +0200

    Dodao aplikaciju na GitHub

---
+++ CekanjeSlobodnogTermina/CekanjeSlobodnogTermina/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CekanjeSlobodnogTermina
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.headPanel = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.PictureBox();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.colorButton = new CekanjeSlobodnogTermina.RoundButton();
+            this.headPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headPanel
+            // 
+            this.headPanel.Controls.Add(this.closeButton);
+            this.headPanel.Location = new System.Drawing.Point(0, 0);
+            this.headPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.headPanel.Name = "headPanel";
+            this.headPanel.Size = new System.Drawing.Size(400, 40);
+            this.headPanel.TabIndex = 0;
+            this.headPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.BackgroundImage = global::CekanjeSlobodnogTermina.Properties.Resources.close;
+            this.closeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.closeButton.Location = new System.Drawing.Point(370, 10);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(20, 20);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.TabStop = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.closeButton.MouseLeave += new System.EventHandler(this.closeButton_MouseLeave);
+            this.closeButton.MouseHover += new System.EventHandler(this.closeButton_MouseHover);
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Carme", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.White;
+            this.headerLabel.Location = new System.Drawing.Point(99, 55);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(202, 24);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "The last available date";
+            this.headerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.Font = new System.Drawing.Font("Carme", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.ForeColor = System.Drawing.Color.White;
+            this.dateLabel.Location = new System.Drawing.Point(15, 80);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateLabel.Size = new System.Drawing.Size(370, 80);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "00/00/0000";
+            this.dateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("Carme", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.White;
+            this.statusLabel.Location = new System.Drawing.Point(49, 160);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(320, 21);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "Searching for available appointments.";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // colorButton
+            // 
+            this.colorButton.BackColor = System.Drawing.Color.Transparent;
+            this.colorButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("colorButton.BackgroundImage")));
+            this.colorButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.colorButton.CornerRadius = 100;
+            this.colorButton.Font = new System.Drawing.Font("Carme", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorButton.ForeColor = System.Drawing.Color.White;
+            this.colorButton.Location = new System.Drawing.Point(100, 220);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.RoundCorners = ((CekanjeSlobodnogTermina.Corners)((((CekanjeSlobodnogTermina.Corners.TopLeft | CekanjeSlobodnogTermina.Corners.TopRight) 
+            | CekanjeSlobodnogTermina.Corners.BottomLeft) 
+            | CekanjeSlobodnogTermina.Corners.BottomRight)));
+            this.colorButton.Size = new System.Drawing.Size(200, 40);
+            this.colorButton.TabIndex = 1;
+            this.colorButton.Text = "I want another color!";
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 280);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.headerLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.headPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Cekanje Slobodnog Termina";
+            this.headPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headPanel;
+        private RoundButton colorButton;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.PictureBox closeButton;
+    }
+}
+
---
+++ CekanjeSlobodnogTermina/CekanjeSlobodnogTermina/Form1.cs
@@ -0,0 +1,228 @@
+﻿using CekanjeSlobodnogTermina.Properties;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CekanjeSlobodnogTermina
+{
+    public partial class MainForm : Form
+    {
+        WebBrowser chrome;
+        WebBrowser fireFox;
+        WebBrowser opera;
+
+        System.Windows.Forms.Timer serachWebsiteTimer = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer waitTimer = new System.Windows.Forms.Timer();
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void MainForm_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+          int nLeftRect,
+          int nTopRect,
+          int nRightRect,
+          int nBottomRect,
+          int nWidthEllipse,
+          int nHeightEllipse
+        );
+
+        private const int CS_DROPSHADOW = 0x00020000;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+
+        private void setStartUpLocation()
+        {
+            Rectangle hostScreen = Screen.FromControl(this).Bounds;
+            this.Location = new Point(hostScreen.Width - this.Width - 40, 40);
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.Icon = Resources.icon;
+            this.Opacity = .85;
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 5, 5));
+
+            this.BackColor = Color.FromArgb(61, 201, 154);
+            headPanel.BackColor = Color.FromArgb(41, 181, 134);
+            colorButton.BackColor = this.BackColor;            
+
+            waitTimer.Interval = 500;
+            waitTimer.Tick += new EventHandler(waitTimer_Tick);
+            waitTimer.Start();
+
+            serachWebsiteTimer.Interval = 60000;
+            serachWebsiteTimer.Tick += new EventHandler(serachWebsiteTimer_Tick);
+            serachWebsiteTimer.Start();
+            serachWebsiteTimer_Tick(null,null);
+
+            setStartUpLocation();           
+        }
+
+        private void serachWebsiteTimer_Tick(object sender, EventArgs e)
+        {
+            chrome = new WebBrowser();
+            chrome.Navigate("http://gim.ftn.uns.ac.rs/Prijava");
+            chrome.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(chrome_DocumentCompleted);
+        }
+
+        private void chrome_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                ((WebBrowser)sender).Document.GetElementsByTagName("input").GetElementsByName("korisnickoIme")[0].SetAttribute("value", "E3 107/2014");
+                ((WebBrowser)sender).Document.GetElementsByTagName("input").GetElementsByName("lozinka")[0].SetAttribute("value", "sifrazagim");
+
+                foreach (HtmlElement item in ((WebBrowser)sender).Document.GetElementsByTagName("input"))
+                {
+                    if (item.GetAttribute("value") == "Prijava")
+                    {
+                        item.InvokeMember("click");
+                        break;
+                    }
+                }                
+            }
+            catch
+            {
+                fireFox = new WebBrowser();
+                fireFox.Navigate("http://gim.ftn.uns.ac.rs/IzmenaZakazanogTermina");
+                fireFox.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(fireFox_DocumentCompleted);
+            }
+        }
+
+        private void fireFox_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string lastDate = "";
+
+            foreach (HtmlElement item in ((WebBrowser)sender).Document.GetElementsByTagName("select").GetElementsByName("datum")[0].All)
+            {
+                lastDate = item.GetAttribute("value");
+            }
+
+            dateLabel.Text = lastDate.Replace('.','/');
+
+            opera = new WebBrowser();
+            opera.Navigate("http://gim.ftn.uns.ac.rs/IzmenaZakazanogTermina?nastavnik=1&datum=" + lastDate);
+            opera.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(opera_DocumentCompleted);            
+        }
+
+        private void opera_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            bool foundSpanMessage = false;
+
+            foreach(var value in ((WebBrowser)sender).Document.GetElementsByTagName("span"))
+            {
+                if (((HtmlElement)value).InnerText.Contains("zauzeti"))
+                    foundSpanMessage = true;                
+            }
+
+            if (!foundSpanMessage)
+            {
+                waitTimer.Stop();
+                statusLabel.TextAlign = ContentAlignment.MiddleCenter;
+
+                System.Windows.Forms.Timer waitTimer2 = new System.Windows.Forms.Timer();
+                waitTimer2.Tick += new EventHandler(waitTimer2_Tick);
+                waitTimer2.Interval = 1000;
+                waitTimer2.Start();
+
+                statusLabel.Text = "Found available appointment!";
+            }
+        }
+
+        private void waitTimer2_Tick(object sender, EventArgs e)
+        {
+            statusLabel.Visible = !statusLabel.Visible;
+
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = "feedback.mp3";
+            wplayer.controls.play();
+        }
+
+        private void waitTimer_Tick(object sender, EventArgs e)
+        {
+            uint dotCounter = 0;
+            foreach(char c in statusLabel.Text)
+            {
+                if (c == '.')
+                    dotCounter++;
+            }
+
+            if(dotCounter == 3)
+                statusLabel.Text = statusLabel.Text.Substring(0,statusLabel.Text.Length-2);
+            else
+                statusLabel.Text += ".";
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor == Color.FromArgb(61, 201, 154))
+            {
+                this.BackColor = Color.FromArgb(205, 75, 116);
+                headPanel.BackColor = Color.FromArgb(185, 55, 96);
+                colorButton.BackColor = this.BackColor;
+            }
+            else if(this.BackColor == Color.FromArgb(205, 75, 116))
+            {
+                this.BackColor = Color.FromArgb(77, 73, 147);
+                headPanel.BackColor = Color.FromArgb(57, 53, 127);
+                colorButton.BackColor = this.BackColor;
+            }
+            else if(this.BackColor == Color.FromArgb(77, 73, 147))
+            {
+                this.BackColor = Color.FromArgb(233, 76, 61);
+                headPanel.BackColor = Color.FromArgb(213, 56, 41);
+                colorButton.BackColor = this.BackColor;
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(61, 201, 154);
+                headPanel.BackColor = Color.FromArgb(41, 181, 134);
+                colorButton.BackColor = this.BackColor;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void closeButton_MouseHover(object sender, EventArgs e)
+        {
+            closeButton.Cursor = Cursors.Hand;
+            closeButton.BackgroundImage = Resources.close_hover;
+        }
+
+        private void closeButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.Cursor = Cursors.Default;
+            closeButton.BackgroundImage = Resources.close;
+        }
+    }
+}
